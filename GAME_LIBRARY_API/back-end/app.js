baa056621d95db7a364efd200765e10133ea5ab3
DiegoFatVega <fatvegadiego@gmail.com>
2026-03-08 21:56:33 +0100

    Create route show

--- GAME_LIBRARY_API/back-end/app.js
+++ GAME_LIBRARY_API/back-end/app.js
@@ -5,9 +5,22 @@ const app = express()
 
 const port = 3000
 
-//server
-
+//rotta index
 app.get("/games", (req, res) => res.json(games))
 
-console.log("Ciao")
+
+//rotta Show
+app.get("/games/:id", (req, res) => {
+    //prendere id
+    const id = parseInt(req.params.id)
+    const game = games.find(game => game.id === id)
+    if (!game) {
+        return res.status(404).json({
+            error: "il Gioco che hai cercato non esiste"
+        })
+    }
+    res.json(game)
+})
+
+//alla fine.
 app.listen(port, () => console.log(`app Listen on port ${port}`))
