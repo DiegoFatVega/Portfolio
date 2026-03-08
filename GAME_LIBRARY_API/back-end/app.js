d33865964372d5e5db8f2b47fb2a6188b629a3ff
DiegoFatVega <fatvegadiego@gmail.com>
2026-03-08 23:27:23 +0100

    Create route post and put

--- GAME_LIBRARY_API/back-end/app.js
+++ GAME_LIBRARY_API/back-end/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const games = require('./data/games.json')
 const app = express()
+app.use(express.json())//usa il metodo json di express
 
 const port = 3000
 
@@ -27,13 +28,17 @@ app.get("/games/:id", (req, res) => {
 //rotta Delete
 app.delete("/games/:id", (req, res) => {
     const id = parseInt(req.params.id)
+    //cerchi in base all'indice e lo salvi in una variabile
     const gameIndex = games.findIndex(game => game.id === id) //se non trova -1
+    //controlli se la ricerca è andata a buon fine
     if (gameIndex === -1) {
         return res.status(404).json({
             error: "il Gioco che vuoi cancellare non esiste"
         })
     }
+    //salvi il gioco che stai per cancellare
     const deletedGame = games[gameIndex]
+    //cancelli il gioco
     games.splice(gameIndex, 1)
 
     return res.status(200).json({
@@ -41,6 +46,56 @@ app.delete("/games/:id", (req, res) => {
     })
 
 })
+
+//Rotta POST
+app.post("/games", (req, res) => {
+    const body = req.body //corpo della richiesta
+    if (!body.title) {
+        return res.status(400).json({
+            message: "Attenzione! il campo titolo è obbligatorio"
+        })
+    }
+    const newGameId = games[games.length - 1].id + 1 //genero id automaticamente
+    const newGame = {
+        id: newGameId,
+        ...body
+    }
+    games.push(newGame)
+    res.status(201).json({
+        message: "gioco aggiunto con successo",
+        game: newGame
+    })
+
+})
+
+//Rotta PUT
+app.put("/games/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const body = req.body
+    const gameIndex = games.findIndex(game => game.id === id)
+
+    if (gameIndex === -1) {
+        return res.status(404).json({
+            error: "il Gioco che vuoi modificare non esiste"
+        })
+    }
+    if (!body.title) {
+        return res.status(400).json({
+            message: "Attenzione! il campo titolo è obbligatorio"
+        })
+    }
+    const updatedGame = {
+        id,
+        ...body
+    }
+
+    games.splice(gameIndex, 1, updatedGame)
+
+    return res.status(200).json({
+        message: `Il gioco "${updatedGame.title}" è stato aggiornato`
+    })
+})
+
 
 //alla fine.
 app.listen(port, () => console.log(`app Listen on port ${port}`))
