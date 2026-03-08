63c40c07056836ab7ac7e731308be0fb1f8316c4
DiegoFatVega <fatvegadiego@gmail.com>
2026-03-08 22:26:51 +0100

    Create route delete

--- GAME_LIBRARY_API/back-end/app.js
+++ GAME_LIBRARY_API/back-end/app.js
@@ -13,13 +13,33 @@ app.get("/games", (req, res) => res.json(games))
 app.get("/games/:id", (req, res) => {
     //prendere id
     const id = parseInt(req.params.id)
+    //cerca e salva 
     const game = games.find(game => game.id === id)
+    //controlla se esiste
     if (!game) {
         return res.status(404).json({
             error: "il Gioco che hai cercato non esiste"
         })
     }
     res.json(game)
+})
+
+//rotta Delete
+app.delete("/games/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const gameIndex = games.findIndex(game => game.id === id) //se non trova -1
+    if (gameIndex === -1) {
+        return res.status(404).json({
+            error: "il Gioco che vuoi cancellare non esiste"
+        })
+    }
+    const deletedGame = games[gameIndex]
+    games.splice(gameIndex, 1)
+
+    return res.status(200).json({
+        message: `Il gioco ${deletedGame.title} è stato cancellato`
+    })
+
 })
 
 //alla fine.
